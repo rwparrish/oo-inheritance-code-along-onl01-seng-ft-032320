e032713570ceeb5e7d19b00d1e7ffa0f4690b77a
Ryan Parrish <rino3717@gmail.com>
2020-04-08 05:08:17 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -4,8 +4,8 @@ class Vehicle
 
   def initialize(wheel_size, wheel_number)
    @wheel_size = wheel_size
-    @wheel_number = wheel_number
-  end
+  @wheel_number = wheel_number
+end
 
 def go 
   "vrrrrrrrooom!"
